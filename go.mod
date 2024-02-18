06ff93cb4f04ba1e615437ffa512d9d28c2a6312
limitr <kakashihotakimail.ru@gmail.com>
2024-02-19 01:42:03 +0300

    feat: password

--- go.mod
+++ go.mod
@@ -5,16 +5,17 @@ go 1.18
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell v1.4.0 // indirect
-	github.com/gdamore/tcell/v2 v2.6.1-0.20231203215052-2917c3801e73 // indirect
+	github.com/gdamore/tcell/v2 v2.7.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/tview v0.0.0-20240204151237-861aa94d61c8 // indirect
 	github.com/rivo/uniseg v0.4.7-0.20240127222946-601bbb3750c2 // indirect
 	golang.design/x/clipboard v0.7.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.6.0 // indirect
 	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/term v0.9.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- internal/ui/main.go
+++ internal/ui/main.go
@@ -1,8 +1,13 @@
 package ui
 
 import (
+	"errors"
 	"go-crypt-message/internal/core/users"
+	"go-crypt-message/pkg/password"
 	manager "go-crypt-message/pkg/rsa"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
@@ -15,11 +20,45 @@ const (
 	SOME_MESSAGE = 0
 )
 
+func getPassword() string {
+	if _, err := os.Stat("./etc/hash"); errors.Is(err, os.ErrNotExist) {
+		return ""
+	} else {
+		b, _ := ioutil.ReadFile("./etc/hash")
+		return string(b)
+	}
+}
+
 func RunUI(manager *manager.ManagerRSA, userManager *users.UserManager) {
 	app := tview.NewApplication()
 	box := tview.NewFlex()
+	pages := tview.NewPages()
 
 	column := 0
+
+	inputPassword := tview.NewInputField().
+		SetLabel("Enter a password: ").
+		SetFieldWidth(10)
+	inputPassword.
+		SetDoneFunc(func(key tcell.Key) {
+			if key == tcell.KeyEnter {
+				rawPass := inputPassword.GetText()
+				pass := getPassword()
+				if pass == "" {
+					salt, p := password.Encode(rawPass, nil)
+					ioutil.WriteFile("./etc/hash", []byte(salt+" "+p), os.ModePerm)
+				} else {
+					slicePass := strings.Split(pass, " ")
+					if password.Verify(rawPass, slicePass[0], slicePass[1], nil) {
+						pages.SwitchToPage("main")
+					} else {
+						app.Stop()
+					}
+				}
+			}
+		})
+
+	pages.AddPage("login", inputPassword, true, true)
 
 	btnGetMyPublicKey := tview.NewButton("Save public key in clipboard")
 	btnGetMyPublicKey.SetTitleAlign(1)
@@ -28,8 +67,6 @@ func RunUI(manager *manager.ManagerRSA, userManager *users.UserManager) {
 	inputText := tview.NewTextArea().SetPlaceholder("Input text...")
 	tableMessage := tview.NewTable().
 		SetBorders(false).SetFixed(2, 10)
-
-	pages := tview.NewPages()
 
 	userPress := ""
 
@@ -132,12 +169,12 @@ func RunUI(manager *manager.ManagerRSA, userManager *users.UserManager) {
 				tview.NewFlex().SetDirection(tview.FlexColumn).
 					AddItem(tview.NewFlex().SetDirection(tview.FlexRow).AddItem(btnGetMyPublicKey, 0, 1, false).
 						AddItem(flexUserList, 0, 5, false), 0, 1, false).
-					AddItem(tview.NewFlex().SetDirection(tview.FlexRow).AddItem(lableUserName, 0, 1, false).AddItem(tableMessage, 0, 1, false), 0, 1, false),
+					AddItem(tview.NewFlex().SetDirection(tview.FlexRow).AddItem(lableUserName, 0, 1, false).AddItem(tableMessage, 0, 6, false), 0, 1, false),
 				0, 3, false).
 			AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).AddItem(inputText, 0, 5, true).AddItem(btnEncrypt, 0, 1, false).AddItem(btnCrypto, 0, 1, false), 0, 1, true), 0, 1, false)
 
 	pages.AddPage("main", box, true, true)
-	pages.SwitchToPage("main")
+	pages.SwitchToPage("login")
 
 	if err := app.SetRoot(pages, true).EnableMouse(true).SetFocus(pages).Run(); err != nil {
 		panic(err)
